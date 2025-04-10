4bbc9ef5b0c46db9fb67a2f9bc01deea5f6bee16
aqadeer6 <aqadeer6@bradford.ac.uk>
2025-04-10 17:47:32 +0100

    Delete footer.php

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <p>Council Switchboard: 01274 432111</p>
-    <p>Council Address: Britannia House, Hall Ings, Bradford BD1 1HX</p>
-    <button onclick="window.location.href = '/software/pages/privacy-policy.php'">Privacy Policy</button>
-</footer>
